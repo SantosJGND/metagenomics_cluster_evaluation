c34358630ae1c939e41b0d5a7babebf73ee1bde1
SantosJGND <dourado.jns@gmail.com>
2025-09-09 12:30:42 +0100

    adjust wrapup, replace mapping_to_matrix executable with latest version (now with min pairwise dist)

--- utils/wrap_up.py
+++ utils/wrap_up.py
@@ -5,26 +5,27 @@ def parse_args():
     import argparse
     parser = argparse.ArgumentParser(description="Match clade report with reference sequences")
     parser.add_argument("output_dir", type=str, help="workflow output directory with clade report and matched assemblies")
-
+    parser.add_argument("--clade_report", type=str, default="clade_report.tsv", help="clade report file name")
+    parser.add_argument("--matched_assemblies", type=str, default="matched_assemblies.tsv", help="matched assemblies file name")
+    parser.add_argument("--coverage_report", type=str, default="merged_coverage_statistics.tsv", help="coverage report file name")
     args = parser.parse_args()
     return args
 
 def main():
 
     args = parse_args()
-    clade_report = f"{args.output_dir}/clade_report.tsv"
-    matched_assemblies = f"{args.output_dir}/matched_assemblies.tsv"
-    coverage_report = f"{args.output_dir}/coverage_report.tsv"
+    clade_report = f"{args.output_dir}/{args.clade_report}"
+    matched_assemblies = f"{args.output_dir}/{args.matched_assemblies}"
+    coverage_report = f"{args.output_dir}/{args.coverage_report}"
 
-    clade_report = pd.read_csv(clade_report, sep="\\t")
-    clade_report['clade']
+    clade_report = pd.read_csv(clade_report, sep="\t", header=None, names=["clade", "nuniq", "freq", "files"])
     clade_report['files'] = clade_report['files'].str.split(',')
     clade_report = clade_report.explode('files')
 
-    matched_assemblies = pd.read_csv(matched_assemblies, sep="\\t")
+    matched_assemblies = pd.read_csv(matched_assemblies, sep="\t")
     matched_assemblies['filename'] = matched_assemblies['assembly_file'].str.split('/').str[-1]
 
-    coverage_report = pd.read_csv(coverage_report, sep="\\t")
+    coverage_report = pd.read_csv(coverage_report, sep="\t")
 
     def find_assembly_mapping(row):
         accession = row['assembly_accession']
@@ -63,7 +64,7 @@ def main():
     clade_report_with_references = clade_report_with_references[['description', 'taxid', 'assembly_accession', \
             'coverage', 'clade', 'nuniq', 'freq']]
 
-    clade_report_with_references.to_csv("clade_report_with_references.tsv", sep="\\t", index=False)
+    clade_report_with_references.to_csv(f"{args.output_dir}/clade_report_with_references.tsv", sep="\t", index=False)
 
 
 if __name__ == "__main__":
